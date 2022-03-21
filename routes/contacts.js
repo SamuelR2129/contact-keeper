fc5e3c9dd1a129ba89a4308fd2e86d8f6ba14396
Sam Reedy <S.Reedy@nib.com.au>
2022-03-21 17:03:11 +1100

    finished express end, added way to delete contacts, set up react app

--- routes/contacts.js
+++ routes/contacts.js
@@ -92,8 +92,24 @@ router.put("/:id", auth, async (req, res) => {
 // @route DELETE api/contacts/:id
 // @desc delete contacts
 ///@access private
-router.delete("/:id", (req, res) => {
-  res.send("delete contacts");
+router.delete("/:id", auth, async (req, res) => {
+  try {
+    let contact = await Contact.findById(req.params.id);
+
+    if (!contact) return res.status(404).json({ msg: "contact not found" });
+
+    //make sure user owns contact
+    if (contact.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: "not authorized" });
+    }
+
+    await Contact.findByIdAndRemove(req.params.id);
+
+    res.json({ msg: "contact removed" });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("server error");
+  }
 });
 
 module.exports = router;
